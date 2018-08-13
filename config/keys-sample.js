badcc462052bf9f0ef1222741419b9b7cdd85b34
Rabindra Joshi <therj@users.noreply.github.com>
2018-08-13 09:44:57 +0545

    set up keys

---
+++ config/keys-sample.js
@@ -0,0 +1,12 @@
+// Grab clientID and secret from https://console.developers.google.com/
+
+const clientID = '57489590558-hgq9rja7wwhpcfcdtqh7g0v5.apps.googleusercontent.com'
+const clientSecret = 'krnhdurdmghutrUvfd'
+
+
+module.exports = {
+  google: {
+    clientID,
+    clientSecret
+  }
+}
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,10 +1,11 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20')
+const keys = require('./keys').
 
 passport.use(
   new GoogleStrategy({
-  // options for strategy
-
+    // options for strategy
+clientID: keys.google.clientID, clientSecret:keys.google.clientSecret
   }), () => {
     // passport callback function
   }
