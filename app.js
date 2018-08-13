5a32bf3c6a6f64870a6790fb80735de337dc313c
Rabindra Joshi <therj@users.noreply.github.com>
2018-08-13 09:08:31 +0545

    set up all auth routes

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
+
+const authRoutes = require('./routes/auth-routes')
+
 const app = express()
 
 //Set view engine 
 app.set('view engine', 'ejs')
+
+// Setup routes
+app.use('/auth', authRoutes)
 
 // Create home route
 app.get('/', (req, res) => {
---
+++ routes/auth-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router
+
+//auth login
+router.get('/login', (req, res) => {
+  res.render('login', {
+    user: req.user
+  })
+})
+
+router.get('/logout', (req, res) => {
+  res.send('logging out')
+})
+
+router.get('/google', (req, res) => {
+  res.send('logging in with googles')
+})
+
+
+module.exports = router
