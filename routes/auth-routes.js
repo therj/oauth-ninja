25d93fde9bb22086cc906feab1b5a09b5d8d68c5
Rabindra Joshi <therj@users.noreply.github.com>
2018-08-13 09:13:48 +0545

    fixed Router() on auth

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,10 +1,8 @@
-const router = require('express').Router
+const router = require('express').Router()
 
 //auth login
 router.get('/login', (req, res) => {
-  res.render('login', {
-    user: req.user
-  })
+  res.render('login', {user:req.user})
 })
 
 router.get('/logout', (req, res) => {
